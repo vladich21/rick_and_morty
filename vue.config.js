01815339048c8e9a345d074740f19167f6aeb1de
Vladislav <vladkoval039@gmail.com>
2024-05-24 22:43:17 +0300

    Deploy to GitHub Pages

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/rick_and_morty/' : '/'
-  };
-  
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/rick_and_morty/'  // Убедитесь, что это путь к вашему репозиторию на GitHub
+    : '/'
+}
